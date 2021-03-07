361e8f3990e2133891f56c09c290ec20ce418e5a
JianMin-Xie <854377742@qq.com>
2021-03-07 17:03:42 +0800

    4.配置解析：mappers+properties+typeAlias+settings

---
+++ mybatis-02/src/main/java/com/xjm/dao/UserMapper.java
@@ -0,0 +1,37 @@
+package com.xjm.dao;
+
+import com.xjm.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+   //查询全部用户
+   List<User> selectUser();
+
+   //根据id查询用户
+   User selectUserById(int id);
+
+   //通过密码和名字查询用户
+   User selectUserByNP(@Param("username") String username, @Param("pwd") String pwd);
+
+   User selectUserByNP2(Map<String, Object> map);
+
+   //添加一个用户
+   int addUser(User user);
+
+   //根据id删除用户
+   int deleteUser(int id);
+
+   //修改一个用户
+   int updateUser(User user);
+
+   //模糊查询
+   List<User> selectLike(String value);
+
+   //模糊查询 #{} 不会引起sql注入
+   List<User> selectLike2(String value);
+   //模糊查询 ${} 会引起sql注入
+   List<User> selectLike3(String value);
+}
---
+++ mybatis-02/src/test/java/com/xjm/dao/UserMapperTest.java
@@ -0,0 +1,134 @@
+package com.xjm.dao;
+
+import com.xjm.pojo.User;
+import com.xjm.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Panda
+ * @create 2021-03-05 20:06
+ */
+public class UserMapperTest {
+
+    @Test
+    public void test() {
+
+        SqlSession session = MybatisUtils.getSession();
+        //方法一:
+        //List<User> users = session.selectList("com.kuang.mapper.UserMapper.selectUser");
+        //方法二:
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUser();
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+        session.close();
+
+    }
+
+    @Test
+    public void testSelectUserById() {
+        SqlSession session = MybatisUtils.getSession();  //获取SqlSession连接
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.selectUserById(1);
+        System.out.println(user);
+        session.close();
+    }
+
+    @Test
+    public void testselectUserByNP() {
+        SqlSession session = MybatisUtils.getSession();  //获取SqlSession连接
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.selectUserByNP("狂神","123456");
+        System.out.println(user);
+        session.close();
+    }
+    @Test
+    public void testselectUserByNP2() {
+        SqlSession session = MybatisUtils.getSession();  //获取SqlSession连接
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("username","张三");
+        map.put("pwd","abcdef");
+        User user = mapper.selectUserByNP2(map);
+        System.out.println(user);
+        session.close();
+    }
+
+    @Test
+    public void testAddUser() {
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User(5,"王五","zxcvbn");
+        int i = mapper.addUser(user);
+        System.out.println(i);
+        session.commit(); //提交事务,重点!不写的话不会提交到数据库
+        session.close();
+    }
+
+    @Test
+    public void testDeleteUser() {
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        int i = mapper.deleteUser(5);
+        System.out.println(i);
+        session.commit(); //提交事务,重点!不写的话不会提交到数据库
+        session.close();
+    }
+
+    @Test
+    public void testUpdateUser() {
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.selectUserById(1);
+        user.setPwd("asdfgh");
+        int i = mapper.updateUser(user);
+        System.out.println(i);
+        session.commit(); //提交事务,重点!不写的话不会提交到数据库
+        session.close();
+    }
+
+    //第1种：在Java代码中添加sql通配符。
+    @Test
+    public void testselectLike() {
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        List<User> userList = mapper.selectLike("%李%");
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        session.close();
+    }
+    //第2种：在sql语句中拼接通配符，#{} 不会引起sql注入
+    @Test
+    public void testselectLike2() {
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+
+        List<User> userList = mapper.selectLike2("李");
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        session.close();
+    }
+    //第2种：在sql语句中拼接通配符，${} 会引起sql注入
+    @Test
+    public void testselectLike3() {
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+
+        List<User> userList = mapper.selectLike3(" or 1=1 #");
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        session.close();
+    }
+
+
+}
